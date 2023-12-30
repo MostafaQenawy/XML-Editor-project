28b42ce992c0699142419f63e5574319620a9dec
MostafaQenawy <130760077+MostafaQenawy@users.noreply.github.com>
2023-12-29 20:42:46 -0800

    Upload the Network Analysis header file

---
+++ Network_Analysis.h
@@ -0,0 +1,19 @@
+#ifndef Network_Analysis
+#define Network_Analysis
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void mostInfluencerUser(vector<User> users);
+
+void mostActiveUser(vector<User> users);
+
+void mutualFollowers(vector<User> users);
+
+void SuggestedFollowers(vector<User> users);
+
+
+#endif // Network_Analysis
+
+
