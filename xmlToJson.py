2e861db8a1b63e809421c636c223ff8ba151d312
Rana Amr <ranaamr169@yahoo.com>
2023-12-30 11:47:19 +0200

    Update xmlToJson.py
    Docs: add doc string

--- xmlToJson.py
+++ xmlToJson.py
@@ -5,22 +5,29 @@
 #include <sstream>
 #include <map>
 
+// Struct representing a node in an XML-like structure
 struct Node {
-    std::string name;
-    std::string content;
-    std::vector<Node> children;
+    std::string name;           // Name of the node
+    std::string content;        // Content of the node (for leaf nodes)
+    std::vector<Node> children; // Children nodes
 
+    // Default constructor
     Node() = default;
+
+    // Constructor with a specified name
     explicit Node(std::string n) : name(std::move(n)) {}
 
+    // Add a child node to the current node
     void addChild(const Node& node) {
         children.push_back(node);
     }
 
+    // Check if the node is a leaf (has no children)
     bool isLeaf() const {
         return children.empty();
     }
 
+    // Repeat a string a certain number of times
     std::string repeat(const std::string& str, int times) const {
         std::string result;
         for (int i = 0; i < times; ++i) {
@@ -29,6 +36,7 @@ struct Node {
         return result;
     }
 
+    // Convert the node and its children to a JSON-like string
     std::string toJSON(int indent = 0) const {
         std::stringstream ss;
 
@@ -50,8 +58,10 @@ struct Node {
 
             for (auto it = groupedChildren.begin(); it != groupedChildren.end(); ++it) {
                 if (it->second.size() == 1) {
+                    // Output for a single child with indentation
                     ss << repeat("  ", indent + 1) << "\"" << it->first << "\": " << it->second[0].toJSON(indent + 1);
                 } else {
+                    // Output for multiple children with an array
                     ss << repeat("  ", indent + 1) << "\"" << it->first << "\": [\n";
                     for (size_t i = 0; i < it->second.size(); ++i) {
                         ss << repeat("  ", indent + 2) << it->second[i].toJSON(indent + 2);
@@ -74,6 +84,7 @@ struct Node {
     }
 };
 
+// Parse an XML-like string and construct a tree of nodes
 Node parseXML(const std::string& xml) {
     std::stack<Node> stack;
     std::string temp;
@@ -82,6 +93,7 @@ Node parseXML(const std::string& xml) {
 
     for (char ch : xml) {
         if (ch == '<') {
+            // Handle the beginning of a tag
             if (!temp.empty()) {
                 size_t start = temp.find_first_not_of(" \n\r\t");
                 size_t end = temp.find_last_not_of(" \n\r\t");
@@ -91,7 +103,9 @@ Node parseXML(const std::string& xml) {
             }
             isTag = true;
         } else if (ch == '>') {
+            // Handle the end of a tag
             if (!stack.empty() && temp[0] == '/') {
+                // Pop the completed node from the stack
                 Node completedNode = stack.top();
                 stack.pop();
                 if (stack.empty()) {
@@ -100,13 +114,16 @@ Node parseXML(const std::string& xml) {
                     stack.top().addChild(completedNode);
                 }
             } else {
+                // Push a new node onto the stack
                 stack.push(Node(temp));
             }
             temp.clear();
             isTag = false;
         } else if (!isTag && ch == '\n') {
+            // Skip newline characters
             continue;
         } else {
+            // Accumulate characters within a tag
             temp += ch;
         }
     }
