f8fc7f0f376cf59ef68c8336e1da8555d23564e4
Salma20020418 <132143243+Salma20020418@users.noreply.github.com>
2024-01-08 16:16:25 +0200

    Add files via upload

---
+++ postsearch.h
@@ -0,0 +1,9 @@
+#ifndef POSTSEARCH_H
+#define POSTSEARCH_H
+#include <iostream>
+#include <string>
+
+  std::string getTopicBodyFromString(const std::string& xmlContent, const std::string& topicName);
+
+
+#endif // POSTSEARCH_H
